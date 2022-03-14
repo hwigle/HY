dc79a79cee9d9bfe0ca5368ae355087a92d124bc
진휘용 <@hwigle>
2022-03-14 23:00:05 +0900

    Homework

--- Homework/src/com/edu/MathExample.java
+++ Homework/src/com/edu/MathExample.java
@@ -5,23 +5,24 @@ import java.util.Scanner;
 public class MathExample {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
-		int num1, num2, sum, 합, 차, 곱, 몫, 나머지 ;
+		int num1, num2, n1, n2, n3, n4, n5;
+		
 		System.out.println("첫 번째 숫자");
 		num1 = scn.nextInt();
 		System.out.println("두 번째 숫자");
 		num2 = scn.nextInt();
-		합 = num1 + num2;
-		차 = num1 - num2;
-		곱 = num1 * num2;
-		몫 = num1 / num2;
-		나머지 = num1 % num2;
+		n1 = (int)(num1 + num2);
+		n2 = (int)(num1 - num2);
+		n3 = (int)(num1 * num2);
+		n4 = (int)(num1 / num2);
+		n5 = (int)(num1 % num2);
 		
-		System.out.println("두 수의 합: " + 합);
-		System.out.println("두 수의 차: " + 차);
-		System.out.println("두 수의 곱: " + 곱);
-		System.out.println("두 수의 몫: " + 몫);
-		System.out.println("두 수의 나머지: " + 나머지);
-		
+		System.out.println("두 수의 합: " + n1);
+		System.out.println("두 수의 차: " + n2);
+		System.out.println("두 수의 곱: " + n3);
+		System.out.println("두 수의 몫: " + n4);
+		System.out.println("두 수의 나머지: " + n5);
+		scn.close();
 	}
 
 }
