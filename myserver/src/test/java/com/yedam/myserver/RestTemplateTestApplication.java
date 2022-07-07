fb2362bab498da7463566031c126e843505a16c6
hy <jhy7041@gmail.com>
2022-07-07 09:09:08 +0900

    0707 테스트 api

---
+++ myserver/src/test/java/com/yedam/myserver/RestTemplateTestApplication.java
@@ -0,0 +1,24 @@
+package com.yedam.myserver;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class RestTemplateTestApplication {
+
+	public static void main(String[] args) {
+
+		String url = "http://apis.data.go.kr/1471000/FoodNtrIrdntInfoService1/getFoodNtrItdntList1?ServiceKey=oJonnVXeZPAvAAYFbUy03E9vN6uvbQDKxN5FSfTxsS4JOMqj6BzMZJBHG8Le5k/hhHoaIGjJMHQTL45zel3y6A==&desc_kor=%EB%B0%94%EB%82%98%EB%82%98%EC%B9%A9&type=json";
+		RestTemplate restTemplate = new RestTemplate();
+
+		JsonNode res = restTemplate.getForObject(url, JsonNode.class);
+
+		System.out.println(res.get("body").get("items").get(0).get("DESC_KOR"));
+	}
+
+}
