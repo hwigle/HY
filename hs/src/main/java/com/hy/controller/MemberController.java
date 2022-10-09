44fed3075ee8885837fe1f552f2a47234babf8c2
hwigle <jhy7041@gmail.com>
2022-10-10 01:21:40 +0900

    10/09 회원가입 시 비밀번호 암호화 로그인 시 암호화된 비밀번호 인식 로그아웃 동기식, 비동기식 두가지 구현 헤더 구현 관리자 페이지 따로 구현

--- hs/src/main/java/com/hy/controller/MemberController.java
+++ hs/src/main/java/com/hy/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.validation.BindingResult;
@@ -30,26 +31,32 @@ public class MemberController {
 
 	@Autowired
 	private JavaMailSender mailSender;
+	
+	@Autowired
+    private BCryptPasswordEncoder pwEncoder;
 
 	// 로그 기록을 남기기 위해서 Logger 클래스 인 logger 변수를 선언
 	private static final Logger logger = LoggerFactory.getLogger(BookController.class);
 
 	// 회원가입 페이지 이동
-	@RequestMapping(value = "join", method = RequestMethod.GET)
+	@RequestMapping(value = "/join", method = RequestMethod.GET)
 	public void joinGet() {
 		logger.info("회원가입 페이지 진입");
 	}
 
 	// 회원가입
 	@RequestMapping(value = "/join", method = RequestMethod.POST)
-	public String joinPost(MemberVO member) throws Exception {
-		logger.info("join 진입");
+	public String joinPOST(MemberVO member) throws Exception {
+		String rawPw = ""; 		// 인코딩 전 비밀번호
+		String encodePw = ""; 	// 인코딩 후 비밀번호
 
-		// 회원가입 서비스 실행
+		rawPw = member.getMemberPw(); 		// 비밀번호 데이터 얻음
+		encodePw = pwEncoder.encode(rawPw); // 비밀번호 인코딩
+		member.setMemberPw(encodePw); 		// 인코딩된 비밀번호 member객체에 다시 저장
+
+		/* 회원가입 쿼리 실행 */
 		service.memberJoin(member);
-
-		logger.info("join service 성공");
-
+	      
 		return "redirect:/main";
 	}
 
@@ -57,8 +64,6 @@ public class MemberController {
 	@RequestMapping(value = "/memberIdChk", method = RequestMethod.POST)
 	@ResponseBody
 	public String memberIdChkPOST(String memberId) throws Exception {
-
-		logger.info("memberIdChk() 진입");
 
 		int result = service.idCheck(memberId);
 
@@ -113,17 +118,56 @@ public class MemberController {
 	public String loginPOST(HttpServletRequest request, MemberVO member, RedirectAttributes rttr) throws Exception{
         
 		HttpSession session = request.getSession();
-		
-		MemberVO loginVO = service.memberLogin(member);
-		
-		if(loginVO == null) {
-			int result = 0;
-			rttr.addFlashAttribute("result",result);
-			return "redirect:/member/login";
-		}
+        String rawPw = "";
+        String encodePw = "";
         
-		session.setAttribute("member", loginVO);
-		
-        return "redirect:/main";
+		MemberVO loginVO = service.memberLogin(member);// 제출한아이디와 일치하는 아이디 있는지 
+        
+        if(loginVO != null) {   // 일치하는 아이디 존재시
+        	
+            rawPw = member.getMemberPw();       	 // 사용자가 제출한 비밀번호
+            encodePw = loginVO.getMemberPw();        // 데이터베이스에 저장한 인코딩된 비밀번호
+            
+            if(true== pwEncoder.matches(rawPw, encodePw)) { // 비밀번호 일치여부 판단
+            	
+            	loginVO.setMemberPw("");  					// 인코딩된 비밀번호 정보 지움
+            	session.setAttribute("member", loginVO);	// session에 사용자의 정보 저장
+            	return "redirect:/main";        			// 메인페이지 이동
+            	
+            } else { 			
+            	
+            	rttr.addFlashAttribute("result", 0);            
+                return "redirect:/member/login";    // 로그인 페이지로 이동
+            }
+            
+        } else {   		        // 일치하는 아이디가 존재하지 않을 시 (로그인 실패)
+            
+        	 rttr.addFlashAttribute("result", 0);            
+             return "redirect:/member/login";    // 로그인 페이지로 이동
+            
+        }
     }
+	
+	// 로그아웃
+	@RequestMapping(value="logout.do", method=RequestMethod.GET)
+	public String logoutMainGET(HttpServletRequest request) throws Exception{
+		
+		HttpSession session = request.getSession();
+		
+		session.invalidate();
+		
+		return"redirect:/main";
+	}
+	
+	// 비동기식 로그아웃
+    @RequestMapping(value="logout.do", method=RequestMethod.POST)
+    @ResponseBody
+    public void logoutPOST(HttpServletRequest request) throws Exception{
+        
+        HttpSession session = request.getSession();
+        
+        session.invalidate();
+        
+    }
+	
 }
--- hs/src/main/webapp/WEB-INF/views/main.jsp
+++ hs/src/main/webapp/WEB-INF/views/main.jsp
@@ -8,12 +8,39 @@
 <meta charset="UTF-8">
 <title>main</title>
 <link rel="stylesheet" href="resources/css/main.css">
+<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
 </head>
 <body>
 <div class="wrapper">
 	<div class="wrap">
 		<div class="top_gnb_area">
-			<h1>gnb area</h1>
+			<ul class="list">
+				<c:if test = "${member == null}">
+	                <li >
+	                    <a href="/member/login">로그인</a>
+	                </li>
+	                <li>
+	                    <a href="/member/join">회원가입</a>
+	                </li>
+                </c:if>
+                <c:if test="${member != null }">  
+                	<c:if test="${member.adminCk == 1 }">
+                        <li><a href="/admin/main">관리자 페이지</a></li>
+                    </c:if>                  
+                    <li>
+                        <a id="gnb_logout_button">로그아웃</a>
+                    </li>
+                    <li>
+                        마이룸
+                    </li>
+                    <li>
+                        장바구니
+                    </li>
+                </c:if>    
+                <li>
+                    고객센터
+                </li>            
+            </ul>   
 		</div>
 		<div class="top_area">
 			<div class="logo_area">
@@ -35,6 +62,7 @@
                         <span>회원 : ${member.memberName}</span>
                         <span>충전금액 : <fmt:formatNumber value="${member.money }" pattern="\#,###.##"/></span>
                         <span>포인트  : <fmt:formatNumber value="${member.point }" pattern="#,###" /></span>
+                        <a href="/member/logout.do">로그아웃</a>
                     </div>
                 </c:if>
  
@@ -49,5 +77,22 @@
 		</div>
 	</div>
 </div>
+
+<script>
+
+	/* gnb_area 로그아웃 버튼 작동 */
+    $("#gnb_logout_button").click(function(){
+        $.ajax({
+            type:"POST",
+            url:"/member/logout.do",
+            success:function(data){
+                alert("로그아웃 성공");
+                document.location.reload();     
+            } 
+        }); // ajax
+    });
+    
+</script>
+
 </body>
 </html>
